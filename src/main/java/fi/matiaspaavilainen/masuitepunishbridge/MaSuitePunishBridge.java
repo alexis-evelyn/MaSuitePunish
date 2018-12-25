c88c15b082ad3e49733e254c1c702a01dd7861f0
Masa <masa@simasasolutions.com>
2018-12-25 17:32:26 +0200

    Initial commit

---
+++ src/main/java/fi/matiaspaavilainen/masuitepunishbridge/MaSuitePunishBridge.java
@@ -0,0 +1,17 @@
+package fi.matiaspaavilainen.masuitepunishbridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaSuitePunishBridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
